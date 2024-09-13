f919f29e0946051176be6439d1be5f6b683caf52
ROCK478 <nikola.tesla.1973@mail.ru>
2024-09-13 18:25:41 +0500

    DialogManager

---
+++ GameJam2024Ufa/Assets/Scripts/DialogManager.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour
+{
+    public GameObject windowDialog; // окно диалога
+    public GameObject pressFforTalk; //подсказка для нажатия клавиши
+    public TextMeshProUGUI textDialog; // текст диалога
+    public Button button; //кнопка далее
+    public bool GoTalk = false; //кнопка нажалась
+    public string[] message; //сообщения
+    private int NumberDialog = 0; //номер диалога
+    public GameObject[] SpeakPersons; // говорящие персонажи по порядку
+    private GameObject SpeakPersonNow; // говорящий персонаж сейчас
+    private Vector2 rememberFirstScale; // предыдущий размер персонажа
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (!GoTalk)
+            {
+                pressFforTalk.SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                pressFforTalk.SetActive(false);
+                GoTalk = true;
+                Time.timeScale = 0f;
+                windowDialog.SetActive(true);
+                textDialog.text = message[NumberDialog];
+                SpeakPersonNow = SpeakPersons[NumberDialog];
+                rememberFirstScale = SpeakPersonNow.transform.localScale;
+                SpeakPersonNow.transform.localScale *= 1.5f;
+                button.gameObject.SetActive(true);
+            }
+            if (GoTalk)
+            {
+                if (NumberDialog == message.Length - 1)
+                {
+                    button.gameObject.SetActive(false);
+                    Time.timeScale = 1f;
+                }
+                else
+                {
+                    button.gameObject.SetActive(true);
+                    button.onClick.AddListener(NextDialog);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (pressFforTalk != null)
+        {
+            pressFforTalk.SetActive(false);
+        }
+        if (windowDialog != null)
+        {
+            windowDialog.SetActive(false);
+        }
+    }
+
+    public void NextDialog()
+    {
+        NumberDialog++;
+        textDialog.text = message[NumberDialog];
+        SpeakPersonNow.transform.localScale = rememberFirstScale;
+        SpeakPersonNow = SpeakPersons[NumberDialog];
+        rememberFirstScale = SpeakPersonNow.transform.localScale;
+        SpeakPersonNow.transform.localScale *= 1.5f;
+        if (NumberDialog == message.Length - 1)
+        {
+            button.gameObject.SetActive(false);
+            Time.timeScale = 1f;
+        }
+    }
+
+
+}
